5478fb3817886bd4d1a059e29b46d8fa261fb4d4
João Marcelo Dantas <jmgrd98@gmail.com>
2022-08-24 09:20:23 -0300

    initial commit

---
+++ calculator-app/src/App.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [screenValue, setScreenValue] = useState<string>('')
+  const [screenResult, setScreenResult] = useState<number>(0)
+  const [operation, setOperation] = useState<boolean>(false)
+
+  function cleanScreen(){
+    setScreenValue('')
+    setScreenResult('0')
+  }
+
+  const addDigitScreen = (digit:string) => {
+    if(screenValue.length >= 15){
+      return
+    }
+    if((digit === '+' || digit === '-' || digit === 'x' || digit === '/')
+    && operation){
+      setOperation(false)
+      setScreenValue(prevstate => prevstate+digit.toString())
+    }
+    if(operation){
+      setOperation(false)
+      setScreenValue(digit.toString())
+      return
+    }
+    setScreenValue(prevstate => prevstate+digit.toString())
+    setScreenResult(digit.toString())
+  }
+
+  return (
+    <div className="App">
+      <body>
+        
+        <main>
+
+          <div id="output">
+
+            <p>{screenValue}</p>
+            <p>{screenResult.toString()}</p>
+
+          </div>
+          <table>
+            <tr>
+              <td><button onClick={cleanScreen}>C</button></td>
+              <td><button>M</button></td>
+              <td><button onClick={() => addDigitScreen('/')}>/</button></td>
+              <td><button onClick={() => addDigitScreen('*')}>X</button></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => addDigitScreen('7')}>7</button></td>
+              <td><button onClick={() => addDigitScreen('8')}>8</button></td>
+              <td><button onClick={() => addDigitScreen('9')}>9</button></td>
+              <td><button onClick={() => addDigitScreen('-')}>-</button></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => addDigitScreen('4')}>4</button></td>
+              <td><button onClick={() => addDigitScreen('5')}>5</button></td>
+              <td><button onClick={() => addDigitScreen('6')}>6</button></td>
+              <td><button onClick={() => addDigitScreen('+')}>+</button></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => addDigitScreen('1')}>1</button></td>
+              <td><button onClick={() => addDigitScreen('2')}>2</button></td>
+              <td><button onClick={() => addDigitScreen('3')}>3</button></td>
+              <td rowSpan={2}><button id="equal">=</button></td>
+            </tr>
+            <tr>
+              <td colSpan={2}><button id="zero" onClick={() => addDigitScreen('0')}>0</button></td>
+              <td ><button onClick={() => addDigitScreen('.')}>.</button></td>
+            </tr>
+          </table>
+        </main>
+
+      </body>
+    </div>
+  )
+}
+
+export default App
